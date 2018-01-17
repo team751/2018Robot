50e48ff293fe6e322f185c10cb8738efed950776
kennethakers <32209138+kennethakers@users.noreply.github.com>
2018-01-16 19:08:07 -0800

    Established Serial protocol, fixed some bugs, added comments, added IMU capabilities including getOrientation.
    Velocity and distance traveled methods still need to be added.

--- src/org/team751/arduino/ArduinoDataSender.ino
+++ src/org/team751/arduino/ArduinoDataSender.ino
@@ -1,62 +1,127 @@
-#include <MillisTimer.h>
+/**
+   Authors: Kenny Akers
+   Organization: Team 751 Barn2 Robotics
+   Competition: 2018 FRC
+   Email: kakers19@priorypanther.com
 
-unsigned long requestID;
-char requestToken = '-'; // Character used to separate command from ID number.
+   This Arduino program establishes a Serial communication protocol with the RoboRIO.
+   Currently, it is programmed to work with queries of the format:
+                  <query_token><separator_token><request_ID>
+                  Ex. Q-123
 
-MillisTimer timer = MillisTimer(10);
+   And to respond with the following format:
+                  <request_ID><separator><rotation_data><separator><distance_traveled><separator><velocity_data>
+                  Ex. 123-130-42-80
+
+   The Arduino will send this data 3 times unless it receives the confirmation token from the RoboRIO, at which
+   point it will stop. If it does not receive this token before it has sent the data 3 times, it will stop.
+
+   Methods initializeIMU, recalibrate, and getOrientation were written by Aidan Chandra (achandra@priorypanther.com)
+   and are used with the Adafruit BNO055 breakout board.
+*/
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+unsigned long requestID; // Variable to hold the query ID of the incoming request.
+String separator = "-"; // Character used to separate command from ID number.
+String queryToken = "Q"; // Token used by the RoboRIO to initiate a request.
+String confirmationToken = "OK"; // Token used by the RoboRIO signal that it successfully received the correct response to the query (i.e. both request and response IDs match).
+int sendRepeat = 3; // Variable that denotes how many times the Arduino will send the queried information if it does not receive the confirmation token.
+
+boolean debug = true; // Debug flag.
 
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(10);
-  timer.setInterval(1000);
-  timer.expiredHandler(myTimerFunction);
-  timer.setRepeats(3); // Resend 3 times.
-  timer.start();
+  initializeIMU(debug);
 }
 
 void loop() {
   if (Serial.available() > 0) { // If the RoboRIO has sent a query command...
     String request = Serial.readString();
-    Serial.println(request);
-    if (request.charAt(0) == 'Q' && request.charAt(1) == '-') {
-      request = request.substring(request.indexOf("-") + 1);
+    //Serial.println(request);
+    if (request.startsWith(queryToken + separator)) {
+      request = request.substring(request.indexOf(separator) + 1);
       requestID = request.toInt();
-      Serial.println(requestID);
-      for (int i = 0; i < 3; i++) { // Want to send the data 3 times if RoboRIO doesn't send back "OK-###"
-        sendData();
-      }
+      //Serial.println(requestID);
+      sendData();
+    }
+    else if (Serial.read() == 'c') {
+      recalibrate(debug);
     }
   }
 }
 
-void myTimerFunction(MillisTimer &mt)
-{
-  Serial.print(Constant("Repeat: "));
-  Serial.println(mt.getRemainingRepeats());
-  timer.run();
-}
-
-void sendData() { <<IN PROGRESS>>
-  while (!Serial.readString().equals("OK-" + requestID)) { // While the RoboRIO hasn't sent the right verification token. 
+void sendData() {
+  int count = 0; // Counter so the sensor data is only sent a maximum of 3 times.
+  while (count++ < sendRepeat && !Serial.readString().equals(confirmationToken + separator + String(requestID))) { // While the RoboRIO hasn't sent the right verification token.
     Serial.print(requestID);
     Serial.print("-");
-    Serial.print(getRotation());
+    Serial.print(getOrientation(debug));
     Serial.print("-");
-    Serial.print(getDistanceTraveled());
+    Serial.print(getDistanceTraveled(debug));
     Serial.print("-");
-    Serial.println(getVelocity());
+    Serial.println(getVelocity(debug));
     delay(2000);
   }
 }
 
-int getRotation() {
+void recalibrate(boolean debug) {
+  if (debug) {
+    Serial.println("Recalibrating... HOLD STILL");
+  }
+  initializeIMU(false);
+}
+
+/*
+   Returns the current orientation in degrees
+   debug true will print out verbose debug messages
+
+   Note: A delay is reccommended if this function will be
+    called in quick succession. No more than 100ms is neccessary
+*/
+float getOrientation(boolean debug) {
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  if (debug)
+    Serial.println("Getting orientation (X)");
+  if (debug)
+    Serial.println("    " + String(euler.x()));
+  return (float)euler.x();
+}
+
+
+/*
+   Intitializes the IMU. MUST BE CALLED FIRST
+   debug true will print out verbose debug messages
+   Returns true for success, false for failure
+
+   NOTE: Initializing or reinitializing the sensor RESETS THE POSITION TO 0.
+         IF YOUR SENSOR WAS READING "340" AND YOU RECALIBRATE, 340 WILL NOW BE
+         0, EVEN IF YOU DID NOT MOVE THE SENSOR.
+*/
+boolean initializeIMU(boolean debug) {
+  if (debug)
+    Serial.println("Initializing BNO055");
+  if (!bno.begin()) {
+    Serial.println("    Failed");
+    return false;
+  }
+  if (debug) Serial.println("    Success");
+  //bno.setExtCrystalUse(true);
+  return true;
+}
+
+float getDistanceTraveled(boolean debug) {
 
 }
 
-int getDistanceTraveled() {
+float getVelocity(boolean debug) {
 
 }
 
-int getVelocity() {
 
-}
+
