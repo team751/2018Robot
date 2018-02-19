64b09ea2ba3ae062d4592c55d37d7a51df34a2de
Bobby Youstra <bobbyyoustra@protonmail.com>
2018-02-19 14:47:42 -0800

    Update JoystickDrive.java

--- src/org/team751/commands/JoystickDrive.java
+++ src/org/team751/commands/JoystickDrive.java
@@ -24,12 +24,21 @@ public class JoystickDrive extends Command {
 		if (Robot.drivetrain.isDrivingAutonomously) {
 			return;
 		}
-//		if (Robot.drivetrain.pdp.getVoltage() < Robot.drivetrain.brownoutVoltageThreshold) {
-//			
-//		}
+
 		double x = Robot.oi.driverStick.getRawAxis(4);
 		double y = Robot.oi.driverStick.getRawAxis(5);
-		Robot.robotDrive.arcadeDrive(-y, x, true);
+
+		if (Robot.drivetrain.pdp.getVoltage() < Robot.drivetrain.brownoutVoltageThreshold) {
+			// If the time difference between this brownout and the last
+			// brownout is too long, reduce the power.
+			long currentTime = System.currentTimeMillis();
+			if (currentTime - this.lastBrownout > Robot.drivetrain.brownoutPeriodThreshold) {
+				Robot.robotDrive.arcadeDrive(-y / 2, x / 2, true);
+			}
+			this.lastBrownout = currentTime;
+		} else {
+			Robot.robotDrive.arcadeDrive(-y, x, true);
+		}
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
